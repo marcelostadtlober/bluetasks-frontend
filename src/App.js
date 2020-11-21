d767ff9dfc38798074623a7c5dd900dc80a2bedd
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-11-21 16:10:16 -0300

    Integrando o Bootstrap na aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>BlueTasks</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>BlueTasks</h1>
+
+      </div>
+    );
+  }
 }
 
 export default App;
