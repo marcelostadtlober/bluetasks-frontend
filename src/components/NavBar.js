01e4cee00d666785f04f44fa0f52c1d5cee42968
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-11-21 17:02:48 -0300

    Deixando a barra de navegação funcional e atrativa

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { APP_NAME } from '../constants';
 import NavBarItem from './NavBarItem';
 
 class NavBar extends Component {
@@ -7,19 +8,30 @@ class NavBar extends Component {
         
         this.state = {
             items: [
-                { name: "Item 1", href: "/" },
-                { name: "Item 2", href: "/" },
-                { name: "Item 3", href: "/" },
-                { name: "Item 4", href: "/" }
+                { name: "Listar Tarefas", href: "/" },
+                { name: "Nova Tarefa", href: "/form" }
             ]
         }
     }
     
+    onClickHandler(item) {
+        alert(item.name);
+    }
 
     render() {
         return (
             <div>
-                { this.state.items.map(i => <NavBarItem name={ i.name }/>) }
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand" href="#">{ APP_NAME }</span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <div className="navbar-nav mr-auto">
+                        { this.state.items.map(i => <NavBarItem item={ i } onClick={ this.onClickHandler }/>) }
+                        </div>
+                    </div>
+                </nav>                
             </div>
         );
     }
