65ba84d189ce69ca841cec34b9a4d7944c44f6fa
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-11-24 22:22:07 -0300

    19.5-Processando os cliques nos itens da barra de navegação

--- src/components/NavBarItem.js
+++ src/components/NavBarItem.js
@@ -5,7 +5,7 @@ class NavBarItem extends Component {
     render() {
         return (
             <div>
-                <Link className="nav-item nav-link active" to={ this.props.item.href } onClick={ e => this.props.onClick(this.props.item) }>{ this.props.item.name }</Link>
+                <Link className={`nav-item nav-link ${this.props.item.active ? "active" : ""}`} to={ this.props.item.href } onClick={ e => this.props.onClick(this.props.item) }>{ this.props.item.name }</Link>
             </div>
         );
     }
